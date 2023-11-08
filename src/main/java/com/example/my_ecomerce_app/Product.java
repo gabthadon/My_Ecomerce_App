80d55645ad787eab36d272d28ebdd9eed3dd958d
gabthadon <dzangalist@gmail.com>
2023-11-08 11:06:54 +0000

    Added html template files and static files and also created project table

--- src/main/java/com/example/my_ecomerce_app/Product.java
+++ src/main/java/com/example/my_ecomerce_app/Product.java
@@ -1,4 +1,33 @@
 package com.example.my_ecomerce_app;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Product {
+
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            initialValue = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
+
+    @Id
+    public Long id;
+    public String name;
+    public String description;
+    public String image;
+    public Integer rating;
+
 }
