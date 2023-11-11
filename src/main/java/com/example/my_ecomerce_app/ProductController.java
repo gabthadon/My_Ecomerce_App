910dc74b94417bc9ac56af83df9a3a1da5f75478
gabthadon <dzangalist@gmail.com>
2023-11-11 17:06:34 +0000

    Added functionality to update records in the database

--- src/main/java/com/example/my_ecomerce_app/ProductController.java
+++ src/main/java/com/example/my_ecomerce_app/ProductController.java
@@ -11,6 +11,7 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 
 @Controller
@@ -18,6 +19,7 @@ public class ProductController {
 
     private final ProductRepository productRepository;
     private final Path root = Paths.get("src/main/resources/static/uploads");
+
     @Autowired
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -34,15 +36,15 @@ public class ProductController {
     @PostMapping("/putproduct")
     public String addProduct(@RequestParam("image") MultipartFile image, @RequestParam("name") String name, @RequestParam("description") String description, @RequestParam("rating") Integer rating) {
 
-        String imageUrl = StringUtils.cleanPath(image.getOriginalFilename()) ;
- String imagePath = "uploads/"+imageUrl;
+        String imageUrl = StringUtils.cleanPath(image.getOriginalFilename());
+
 
         try {
             Files.copy(image.getInputStream(), this.root.resolve(image.getOriginalFilename()));
 
             //Save to Database
             Product product = Product.builder()
-                    .image(imagePath)
+                    .image(imageUrl)
                     .name(name)
                     .description(description)
                     .rating(rating)
@@ -51,35 +53,67 @@ public class ProductController {
             productRepository.save(product);
 
         } catch (Exception e) {
-            if (e instanceof FileAlreadyExistsException ) {
+            if (e instanceof FileAlreadyExistsException) {
                 throw new RuntimeException("A file of that name already exists.");
             }
 
             throw new RuntimeException(e.getMessage());
         }
 
-        return "shop";
+        return "redirect:products";
 
     }
 
 
     //View All Products From Admin Panel
     @GetMapping("/products")
-        public String getProducts(Model model){
-       model.addAttribute("product", productRepository.findAll());
+    public String getProducts(Model model) {
+        model.addAttribute("product", productRepository.findAll());
 
         return "admin/productview";
-        }
+    }
 
-        @GetMapping("delete/{id}")
-    public String deleteProduct(@PathVariable Long id){
+    @GetMapping("delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
 
         productRepository.deleteById(id);
         return "redirect:/products";
+    }
+
+    @GetMapping("updateproduct/{description}/{name}/{rating}/{image}/{id}")
+    public String getUpdateProduct(Model model, @PathVariable String description, @PathVariable String name, @PathVariable Integer rating, @PathVariable(required = false) String image, @PathVariable Long id) {
+        model.addAttribute("name", name);
+        model.addAttribute("description", description);
+        model.addAttribute("rating", rating);
+        model.addAttribute("image", image);
+        model.addAttribute("id", id);
+        return "/admin/productupdate";
+    }
+
+    @PostMapping("/update/product")
+    public String updateProduct(@RequestParam String description, @RequestParam String name, @RequestParam Integer rating, @RequestParam MultipartFile image, @RequestParam Long id) {
+
+        String imageUrl = StringUtils.cleanPath(image.getOriginalFilename());
+
+
+        try {
+            Files.copy(image.getInputStream(), this.root.resolve(image.getOriginalFilename()));
+
+            int myProductUpdate = productRepository.updateProductById(description, name, rating, imageUrl, id);
+
+            if (myProductUpdate == 1)
+                return "redirect:/products";
+
+
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file of that name already exists.");
+            }
+
+
         }
 
+        return "redirect:/products";
 
-
-
-
+    }
 }
--- src/main/java/com/example/my_ecomerce_app/ProductRepository.java
+++ src/main/java/com/example/my_ecomerce_app/ProductRepository.java
@@ -1,10 +1,23 @@
 package com.example.my_ecomerce_app;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE product SET description= ?1, name=?2, rating=?3, image=?4 WHERE id=?5",
+
+    nativeQuery = true)
+
+  int  updateProductById(String description, String name, Integer rating, String image, Long id);
+
 
 
 }
